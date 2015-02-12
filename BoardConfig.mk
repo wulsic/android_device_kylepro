3d26652a5ec82a972d50b1a7b494e3a27d5a6ac3
SandPox <sandpox@outlook.com>
2015-02-12 06:39:52 +0000

    First commit

---
+++ BoardConfig.mk
@@ -0,0 +1,150 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/samsung/kylepro/BoardConfigVendor.mk
+
+# Platform
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := hawaii
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a9
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_BOOTLOADER_BOARD_NAME := hawaii
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := kylepro,S7580,GT-S7580,hawaii
+
+# Kernel
+BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_BASE := 0x82000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_OFFSET := 0x00008000
+# https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+TARGET_KERNEL_CONFIG := bcm21664_hawaii_ss_kylepro_rev00_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/kylepro/
+#TARGET_PREBUILT_KERNEL := device/samsung/kylepro/kernel
+
+# PARTITION SIZE
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1210769408
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2373976064
+BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 262144
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/kylepro/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/kylepro/bluetooth/libbt_vndcfg_s7580.txt
+
+# Connectivity - Wi-Fi
+WPA_BUILD_SUPPLICANT := true
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+BOARD_WLAN_DEVICE_REV       := bcm4330
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_MODULE_NAME     := "dhd"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_BAND                   := 802_11_ABG
+BOARD_NO_WIFI_HAL           := true
+BOARD_HAVE_SAMSUNG_WIFI     := true
+
+# Resolution
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+# Hardware rendering
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/samsung/kylepro/configs/egl.cfg
+BOARD_USE_MHEAP_SCREENSHOT := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DEGL_NEEDS_FNW -DHAWAII_HWC
+TARGET_USES_ION := true
+HWUI_COMPILE_FOR_PERF := true
+
+# libutils
+#COMMON_GLOBAL_CFLAGS += -DREFBASE_JB_MR1_COMPAT_SYMBOLS
+
+# libwvm.so
+#COMMON_GLOBAL_CFLAGS += -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
+
+# OpenGL
+BOARD_USE_BGRA_8888 := true
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_ALLOW_SUSPEND_IN_CHARGER := true
+CHARGER_DISABLE_INIT_BLANK := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+BOARD_BATTERY_DEVICE_NAME := "battery"
+
+# healthd
+BOARD_HAL_STATIC_LIBRARIES := libhealthd-kylepro.hawaii
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/samsung/kylepro/ril/
+
+# CWM Recovery, don't mark comment this even you are building TWRP
+#TARGET_RECOVERY_INITRC := 
+BOARD_UMS_LUNFILE := /sys/class/android_usb/f_mass_storage/lun0/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+BOARD_RECOVERY_HANDLES_MOUNT := true
+BOARD_USES_MMCUTILS := false
+BOARD_RECOVERY_ALWAYS_WIPES := false
+BOARD_SUPPRESS_EMMC_WIPE := true
+
+# You must mark this line as comment before building TWRP
+TARGET_RECOVERY_FSTAB := device/samsung/kylepro/cwm.fstab
+
+
+# Uncomment this for building TWRP
+#TW_NO_REBOOT_BOOTLOADER := true
+#DEVICE_RESOLUTION := 480x800
+#TW_INTERNAL_STORAGE_PATH := "/data/media"
+#TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+#RECOVERY_SDCARD_ON_DATA := true
+#BOARD_HAS_NO_REAL_SDCARD := true
+#TW_NO_REBOOT_BOOTLOADER := true
+#TARGET_RECOVERY_FSTAB := device/samsung/kylepro/twrp.fstab
+
+
+# CMHW
+BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/kylepro/cmhw/
+
+# GPS
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/kylepro/include
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/kylepro/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts
---
+++ README.md
@@ -0,0 +1,5 @@
+[UNDERDEVELOPMENT]
+
+Cyanogenmod 11 device tree for Samsung Galaxy Trend Plus (GT-S7580)
+
+Must be placed at [Cyanogenmod11Source]/device/samsung/kylepro/
